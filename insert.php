9e3bbb10fd5cec317d39c29cea4bf974aa304731
Josué Moraes <quijobem@hotmail.com>
2022-06-21 17:11:45 -0300

    Form Conect HTML PHP Msqls
    Conected form user PHP end Msql

---
+++ insert.php
@@ -0,0 +1,67 @@
+<?php
+/* Declarando variáveis ​​e verificando se estão vazias ou não*/
+/*Na linha 4, verificamos se ( $username, $password, $gender, $email, $phoneCode and $phone) está definido ou não. Se eles não estiverem definidos, vamos pular até linha 57. Se estiverem definidos, o código da linha 9 será executado.*/
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['password']) &&
+        isset($_POST['gender']) && isset($_POST['email']) &&
+        isset($_POST['phoneCode']) && isset($_POST['phone'])) {
+        
+
+        /* Usando um código da linha 11 a 16, estamos pegando dados do formulário HTML e armazenando-os em variáveis ​​PHP*/
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $phoneCode = $_POST['phoneCode'];
+        $phone = $_POST['phone'];
+
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "test";
+
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+
+            if ($rnum == 0) {
+                $stmt->close();
+
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$username, $password, $gender, $email, $phoneCode, $phone);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
