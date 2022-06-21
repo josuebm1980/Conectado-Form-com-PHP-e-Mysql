cb4af0cced783875eea541305cd547003185fee3
Josué Moraes <quijobem@hotmail.com>
2022-06-21 18:28:20 -0300

    Update form
    Up date form

--- insert.php
+++ insert.php
@@ -15,28 +15,42 @@ if (isset($_POST['submit'])) {
         $phoneCode = $_POST['phoneCode'];
         $phone = $_POST['phone'];
 
+        /* Fazendo uma conexão com o banco de dados atribuindo valores para variáveis*/
         $host = "localhost";
         $dbUsername = "root";
         $dbPassword = "";
         $dbName = "test";
 
+        /*Então, na linha 25, fazemos uma conexão com o banco de dados e a atribuímos a um a variável $conn */
         $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
+        
+        /* Depois disso, na linha 28 verificamos se nosso banco de dados está conectado. Se não houver conexão, exibimos o erro e saímos. Se um banco de dados estiver conectado, continuamos da linha 34. */
         if ($conn->connect_error) {
             die('Could not connect to the database.');
         }
-        else {
+
+        /* Declare consulta SQL em uma variável
+         Na linha 35, há uma consulta SELECT para selecionar dados de um banco de dados e na linha 36, há uma consulta INSERT para inserir o registro do usuário em um banco de dados. */
+        else { 
             $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
             $Insert = "INSERT INTO register(username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
 
+        /* Usando a declaração prepared statement 
+        Depois disso, estamos usando a instrução prepared statement para executar nossa consulta e proteger nosso banco de dados contra injeção de SQL.*/
+
+        /* Executamos a consulta SELECT
+            Na linha 43 estamos passando um valor de email para uma consulta SELECT e definimos “s” porque email é uma string. 
+            Em uma consulta SELECT, estamos selecionando apenas e-mail, então, na linha 47, capturamos esse valor de e-mail. */
             $stmt = $conn->prepare($Select);
             $stmt->bind_param("s", $email);
             $stmt->execute();
             $stmt->bind_result($resultEmail);
             $stmt->store_result();
             $stmt->fetch();
-            $rnum = $stmt->num_rows;
-
+            $rnum = $stmt->num_rows; /* Na linha 50, armazenamos o número de linhas em uma variável  $rnum do nosso resultado.*/
+            
+        /* Insira os dados do usuário
+            Então, na linha 54, ​​verificamos se nosso resultado contém 0 linhas, se sim, continuamos executando nossa codificação, caso contrário, pulamos para a linha 67 e damos uma mensagem “Alguém já se registra usando este email”. */ 
             if ($rnum == 0) {
                 $stmt->close();
 
